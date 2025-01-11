e9bdebbfd97ec392c4ba017837ef007c71f1c9aa
Guille-coder <guillermoreyen.16@gmail.com>
2025-01-10 22:44:06 -0500

    if, else if, else

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string percentageOfTheUsEr = Console.ReadLine();
+        int grade = int.Parse(percentageOfTheUsEr);
+
+        string letter = "";
+
+        if (grade >= 90 )
+        {
+            letter= "A";
+        }
+        else if (grade >= 80)
+        {
+            letter= "B";
+        }
+        else if (grade >= 70)
+        {
+            letter= "C";
+        }
+        else if (grade >= 60)
+        {
+            letter= "D";
+        }
+        else
+        {
+            letter= "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations, You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Don't be discouraged, Better luck next time!");
+        }
+
     }
 }
